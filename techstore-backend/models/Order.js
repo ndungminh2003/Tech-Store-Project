3fcd9f93f218ae3a150dccb8a9540cc691cd1349
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2023-08-23 13:32:36 +0700

    Create Order.js (ZaloPay)

---
+++ techstore-backend/models/Order.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../provider");
+
+class Order extends Sequelize.Model {}
+Order.init(
+  {
+    apptransid: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    zptransid: {
+      type: Sequelize.STRING,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    amount: {
+      type: Sequelize.BIGINT,
+    },
+    timestamp: {
+      type: Sequelize.BIGINT,
+    },
+    channel: {
+      type: Sequelize.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    modelName: "order",
+  }
+);
+
+module.exports = Order;
