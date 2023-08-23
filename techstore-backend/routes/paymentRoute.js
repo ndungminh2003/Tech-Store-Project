eaf22132612106e2c30c4e61312c80dbc6226668
DuyNguyenProgrammer <124415567+DuyNguyenProgrammer@users.noreply.github.com>
2023-08-23 13:50:08 +0700

    Update paymentRoute.js (ZaloPay)

--- techstore-backend/routes/paymentRoute.js
+++ techstore-backend/routes/paymentRoute.js
@@ -5,4 +5,34 @@ const router = express.Router();
 router.post("/urlpayment", payment);
 router.get("/validate", validatePayment);
 
+const ZaloPay = require("../zalopay");
+
+const OrderRespository = require("../respository/OrderRepository");
+
+router.post("/createorder", async (req, res) => {
+  return res.send(await ZaloPay.CreateOrder(req.body));
+});
+
+router.post("/refund", async (req, res) => {
+  res.send(await ZaloPay.Refund(req.body));
+});
+
+router.get("/getrefundstatus", async (req, res) => {
+  const { mrefundid } = req.query;
+  res.send(await ZaloPay.GetRefundStatus(mrefundid));
+});
+
+router.get("/getbanklist", async (req, res) => {
+  res.send(await ZaloPay.GetBankList());
+});
+
+router.get("/gethistory", async (req, res) => {
+  let { page } = req.query;
+  page = Number(page);
+  page = isNaN(page) ? 1 : page;
+
+  const orders = await OrderRespository.Paginate(page);
+  res.send(orders);
+});
+
 module.exports = router;
